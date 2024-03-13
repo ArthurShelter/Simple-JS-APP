cef47d74fb1697d2fccc689c80fc0671db01f9d8
ArthurShelter <158783244+ArthurShelter@users.noreply.github.com>
2024-03-12 22:02:05 -0700

    Added pokemonList for loop

--- js/scripts.js
+++ js/scripts.js
@@ -33,4 +33,6 @@ let pokemonThree = {
     name: 'Meowth',
     height: 0.4,
     types: 'normal'
-};
+};for (let i = 0; i < pokemonList.length; i++) {
+    document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '</p>');
+}
