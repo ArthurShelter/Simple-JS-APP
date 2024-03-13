e9b8f780ef1bd7e6b82769a565a9a9337c48ec97
ArthurShelter <158783244+ArthurShelter@users.noreply.github.com>
2024-03-12 22:01:23 -0700

    Changed pokemonList
    Changed pokemonList to be an array of objects rather than an array that references objects.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
 let pokemonList = [
     [pokemonOne, pokemonTwo, pokemonThree]
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Fearow',
+        height: 1.2,
+        types: ['flying', 'normal']
+    },
+    {
+        name: 'Meowth',
+        height: 0.4,
         types: ['normal']
+    },
 ];
 
 let pokemonOne = {
