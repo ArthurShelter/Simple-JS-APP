09124f61fa78dcfb9f688b956a38c46045a5ae71
ArthurShelter <158783244+ArthurShelter@users.noreply.github.com>
2024-03-20 19:36:37 -0700

    Changed for loop to foreach

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,7 @@ let pokemonList = [
         types: ['normal']
     },
 ];
-
+/*
 for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height > 1.0) {
         document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '</p>');
@@ -24,4 +24,26 @@ for (let i = 0; i < pokemonList.length; i++) {
     else {
         document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '</p>');
     }
+}
+*/
+pokemonList.forEach(function(pokemon) {
+    if (pokemon.height > 1.0) {
+        document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')' + '</p>');
+        document.write(`That's a big one!`);
+    }
+    else {
+        document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')' + '</p>');
+    }
+});
+
+function divide(dividend, divisor) {
+if (divisor === 0)
+{
+    return 'You are trying to divide by 0.'
+}
+else {
+    let result = dividend / divisor;
+    return result;
+}
+
 }
