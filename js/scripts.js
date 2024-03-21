40b54b9ed591ae90c14a06ca24c847c129fd21f2
ArthurShelter <158783244+ArthurShelter@users.noreply.github.com>
2024-03-20 20:05:50 -0700

    Put pokemonList in an IIFE
    (and added internal IIFE functions)

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,38 @@
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 0.7,
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'Fearow',
-        height: 1.2,
-        types: ['flying', 'normal']
-    },
-    {
-        name: 'Meowth',
-        height: 0.4,
-        types: ['normal']
-    },
-];
-/*
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 1.0) {
-        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '</p>');
-        document.write(`That's a big one!`);
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 0.7,
+            types: ['grass', 'poison']
+        },
+        {
+            name: 'Fearow',
+            height: 1.2,
+            types: ['flying', 'normal']
+        },
+        {
+            name: 'Meowth',
+            height: 0.4,
+            types: ['normal']
+        },
+    ];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-    else {
-        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '</p>');
+
+    function getAll() {
+        return pokemonList;
     }
-}
-*/
-pokemonList.forEach(function(pokemon) {
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
     if (pokemon.height > 1.0) {
         document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')' + '</p>');
         document.write(`That's a big one!`);
