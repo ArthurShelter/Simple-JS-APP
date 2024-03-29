da09766b81d3a3d8a9c37944820ec344b03c9c68
ArthurShelter <158783244+ArthurShelter@users.noreply.github.com>
2024-03-28 18:29:22 -0700

    Commenting out old manual pokemon in array

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,23 @@
 let pokemonRepository = (function () {
     let pokemonList = [
-        {
-            name: 'Bulbasaur',
-            height: 0.7,
-            types: ['grass', 'poison']
-        },
-        {
-            name: 'Fearow',
-            height: 1.2,
-            types: ['flying', 'normal']
-        },
-        {
-            name: 'Meowth',
-            height: 0.4,
-            types: ['normal']
-        },
+        // {
+        //     name: 'Bulbasaur',
+        //     height: 0.7,
+        //     types: ['grass', 'poison']
+        // },
+        // {
+        //     name: 'Fearow',
+        //     height: 1.2,
+        //     types: ['flying', 'normal']
+        // },
+        // {
+        //     name: 'Meowth',
+        //     height: 0.4,
+        //     types: ['normal']
+        // },
     ];
+
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
     function add(pokemon) {
         pokemonList.push(pokemon);
@@ -37,21 +39,64 @@ let pokemonRepository = (function () {
     }
 
     function showDetails(pokemon) {
-        console.log(pokemon);
+        loadDetails(pokemon).then(function () {
+            console.log(pokemon);
+        });
     }
 
     function getAll() {
         return pokemonList;
     }
 
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            json.results.forEach(function (item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+            });
+        }).catch(function (e) {
+            console.error(e);
+        })
+    }
+
+    function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+            return response.json();
+        }).then(function (details) {
+            item.imageUrl = details.sprites.front_default;
+            item.height = details.height;
+            item.types = details.types;
+
+
+        }).catch(function (e) {
+            console.error(e);
+        });
+    }
+
+
     return {
         add: add,
         addListItem: addListItem,
         showDetails: showDetails,
-        getAll: getAll
+        getAll: getAll,
+        loadList: loadList,
+        loadDetails: loadDetails
     };
 
 })();
+
+pokemonRepository.loadList().then(function () {
+    //now the data is loaded...
+    pokemonRepository.getAll().forEach(function (pokemon) {
+        pokemonRepository.addListItem(pokemon);
+    });
+});
 
 pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
@@ -79,5 +124,4 @@ function divide(dividend, divisor) {
         let result = dividend / divisor;
         return result;
     }
-
 }
